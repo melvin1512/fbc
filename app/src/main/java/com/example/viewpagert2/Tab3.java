ca0689d39ebf8c2afe3f06a4eab0398b3dd590fe
H56655 <h56655@C02YK0UJH7VF.local>
2019-06-24 11:51:48 +0800

    kenot work

--- app/src/main/java/com/example/viewpagert2/Tab3.java
+++ app/src/main/java/com/example/viewpagert2/Tab3.java
@@ -1,7 +1,9 @@
 package com.example.viewpagert2;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,29 +11,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.lang.ref.Reference;
+import java.sql.Ref;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Tab3 extends Fragment {
 
+    //choose % upload btn
     Button ch,up;
     ImageView img;
     StorageReference mStorageRef;
+    public Uri imguri;
+    public StorageTask uploadtask;
+    View v;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab3, container,false);
-    }
+        v=inflater.inflate(R.layout.tab3, container,false);
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ch = (Button) ch.findViewById(R.id.selectimg);
-        up = (Button) up.findViewById(R.id.saveimg);
-        img = (ImageView) img.findViewById(R.id.imgview);
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+        ch = (Button) v.findViewById(R.id.selectimg);
+        up = (Button) v.findViewById(R.id.saveimg);
+        img = (ImageView) v.findViewById(R.id.imgview);
 
         ch.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -39,6 +54,55 @@ public class Tab3 extends Fragment {
                 Filechooser();
             }
         });
+
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(uploadtask != null && uploadtask.isInProgress()){
+                    Toast.makeText(getActivity(),"Uploading",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Fileuploader();
+                }
+                Fileuploader();
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    private String getExtension(Uri uri){
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+    }
+
+    private void Fileuploader(){
+        StorageReference storageReference = mStorageRef.child(System.currentTimeMillis()+"."+getExtension(imguri));
+
+        uploadtask = storageReference.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(getActivity(), "Image Upload Success", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+
     }
 
     private void Filechooser(){
@@ -51,8 +115,10 @@ public class Tab3 extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == 1 && resultCode == -1 && data != null && data.getData()){
-//
-//        }
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData()!=null){
+            imguri = data.getData();
+            img.setImageURI(imguri);
+
+        }
     }
 }
